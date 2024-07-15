627c464ce3260f3b51474eccbb89c318990df528
m-aminalizadeh <aminalizadeh@riseup.net>
2024-07-15 15:37:51 +0330

    Add custom colors to the tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,23 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-color': 'hsl(242, 48, 58)',
+        'primary-light-color': 'hsl(243, 100, 82)',
+        'secondary-color': 'hsl(0, 78, 63)',
+        'secondary-light-color': 'hsl(0, 100, 80)',
+        'dark-color-1': 'hsl(237, 100, 4)',
+        'dark-color-2': 'hsl(235, 16, 15)',
+        'dark-color-3': 'hsl(235, 12, 19)',
+        'dark-color-4': 'hsl(236, 11, 27)',
+        'dark-color-5': 'hsl(216, 15, 57)',
+        'light-color-1': 'hsl(0, 0, 100)',
+        'light-color-2': 'hsl(220, 69, 97)',
+        'light-color-3': 'hsl(221, 69, 94)',
+
+      }
+    },
   },
   plugins: [],
 }
